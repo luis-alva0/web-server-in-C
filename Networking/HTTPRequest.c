ff848abbe9e8a53785c15f53c0d099096f53b846
luis-alva0 <luisandres.alvaabanto@gmail.com>
2025-07-21 21:12:37 -0500

    feat: http requests

---
+++ Networking/HTTPRequest.c
@@ -0,0 +1,73 @@
+#include "HTTPRequest.h"
+#include <string.h>
+#include <stdlib.h>
+
+int method_select(char *method)
+{
+    if(strcmp(method, "GET") == 0)
+    {
+        return GET;
+    }
+    else if(strcmp(method, "POST") == 0)
+    {
+        return POST;
+    }
+    else if(strcmp(method, "PUT") == 0)
+    {
+        return PUT;
+    }
+    else if(strcmp(method, "HEAD") == 0)
+    {
+        return HEAD;
+    }
+    else if(strcmp(method, "PATCH") == 0)
+    {
+        return PATCH;
+    }
+    else if(strcmp(method, "DELETE") == 0)
+    {
+        return DELETE;
+    }
+    else if(strcmp(method, "CONNECT") == 0)
+    {
+        return CONNECT;
+    }
+    else if(strcmp(method, "OPTIONS") == 0)
+    {
+        return OPTIONS;
+    }
+    else if(strcmp(method, "TRACE") == 0)
+    {
+        return TRACE;
+    }
+}
+
+struct HTTPRequest http_request_constructor(char *request_string)
+{
+    struct HTTPRequest request;
+    
+    for(int i = 0; i < strlen(request_string) - 1; i++)
+    {
+        if(request_string[i] == '\n' && request_string[i+1] == '\n')
+        {
+            request_string[i + 1] = '|';
+        }
+    }
+
+    char *request_line = strtok(request_string, "\n");
+    char *header_fields = strtok(NULL, "|");
+    char *body = strtok(NULL, "|");
+    
+    char *method = strtok(request_line, " ");
+    request.Method = method_select(method);
+
+    char *URI = strtok(NULL, " ");
+    request.URI = URI;
+
+    char *HTTPVersion = strtok(NULL, " ");
+    HTTPVersion = strtok(HTTPVersion, "/");
+    HTTPVersion = strtok(NULL, "/");
+    request.HTTPVersion = (float)atof(HTTPVersion);
+
+    return request;
+}
---
+++ Networking/HTTPRequest.h
@@ -0,0 +1,29 @@
+#ifndef HTTPRequest_h
+#define HTTPRequest_h
+
+#include <stdio.h>
+
+enum HTTPMethods
+{
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    PATCH,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE
+};
+
+struct HTTPRequest
+{
+    int Method;
+    char *URI;
+    float HTTPVersion;
+
+};
+
+struct HTTPRequest http_request_constructor(char *request_string);
+
+#endif
